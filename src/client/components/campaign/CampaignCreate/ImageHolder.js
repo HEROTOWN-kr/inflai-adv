c67771d75b0c4fbd82a6af25b63ddca8ac92bb7a
Andrian <andriantsoy@gmail.com>
2021-10-28 18:14:45 +0900

    CreateNewCampaign 페이지

--- src/client/components/campaign/CampaignCreate/ImageHolder.js
+++ src/client/components/campaign/CampaignCreate/ImageHolder.js
@@ -110,7 +110,7 @@ function ImageHolder(props) {
         checked: false,
         isMain: 0
       };
-      if (dbImages.length === 0 && index === 0) imageObj.isMain = 1;
+      if (dbImages.length === 0 && images.length === 0 && index === 0) imageObj.isMain = 1;
 
       return imageObj;
     });
--- src/client/components/main/Main.js
+++ src/client/components/main/Main.js
@@ -118,7 +118,7 @@ function Main() {
       />
       <Route
         path="/YoutubeAnalysis"
-        render={renderProps => <YoutubeAnalysis {...renderProps} id={90} />}
+        render={renderProps => <YoutubeAnalysis {...renderProps} id={58} />}
       />
       <Route
         component={NotFound}
